c232122b94c075886a5db0cd397dac7565d0228f
SG <SG.binung715@gmail.com>
2024-07-16 14:59:50 +0200

    welcome modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html>
+<style>
+    p {
+        font-size: 32px;
+        font-weight: 600;
+    }
+</style>
 
 <body>
-    <p>Server Started!</p>
+    <p>Server successfully connected!</p>
 </body>
 
 </html>
