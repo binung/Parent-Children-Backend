7e1cf4c867ee33f492c19a616122e13f2c127d6f
SG <SG.binung715@gmail.com>
2024-07-17 10:37:59 +0200

    setting up boradcasting channels

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('parent.{parentId}', function ($user, $parentId) {
+    return $user->id === $parentId;
+});
+
+Broadcast::channel('child.{childId}', function ($user, $childId) {
+    return $user->id === $childId;
+});
