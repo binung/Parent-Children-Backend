6daa8b57ce32ee7fdcf8a2631c0efc4549f83abe
timon0204 <timonpee60@gmail.com>
2024-07-22 18:03:03 +0300

    'socket_change'

--- app.js
+++ app.js
@@ -46,14 +46,23 @@ app.get('/',(req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Listen for app block events
-  socket.on('app-blocked', (data) => {
-    console.log('Received block-app event:', data);
-    // Broadcast to all clients or a specific room
-    io.emit('app-blocked', data);
+  socket.emit('connection-success', { message: 'Successfully connected to server' });
+
+  socket.on('block-app', (data) => {
+    const { child_id, avatar, package_name, app_usage_time, state, app_name } = data;
+    const created_at = Date.now();
+    const updated_at = Date.now();
+    const query = 'INSERT INTO block_apps (child_id, app_name, state, created_at, updated_at, package_name, avatar, app_usage_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    db.query(query, [child_id, app_name, state, created_at, updated_at, package_name, avatar, app_usage_time], (err, result) => {
+      if (err) {
+        console.error('Error inserting data:', err);
+        socket.emit('block-app-response', { status: 'error', message: 'Error saving app blocking information' });
+      } else {
+        socket.emit('block-app-response', { status: 'success', message: 'App blocking information saved successfully' });
+      }
+    });
   });
 
-  // Handle disconnection
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
@@ -61,7 +70,7 @@ io.on('connection', (socket) => {
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/app', require('./routes/userRoutes'));
-app.use('/api/appUsage', require('./routes/appRoutes'));
+// app.use('/api/appUsage', require('./routes/appRoutes'));
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
