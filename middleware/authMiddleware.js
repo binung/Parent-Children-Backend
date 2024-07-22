3c90fc0b49d0b39ccc1bfa4e3742a4af2d888141
timon0204 <timonpee60@gmail.com>
2024-07-22 06:51:04 +0300

    welcome

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.header('x-auth-token');
+  const authHeader = req.header('Authorization');
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
+  const token = authHeader.split(' ')[1]; 
+  if (!token) {  
+      return res.status(401).json({ message: 'Token not found, authorization denied' });  
+  }  
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
