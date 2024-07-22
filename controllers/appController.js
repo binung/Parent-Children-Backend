92711ab9a57c649014aa1bbfe59a52b9ea95522a
timon0204 <timonpee60@gmail.com>
2024-07-22 17:23:36 +0300

    'socket_change'

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,9 +2,8 @@ const { createApp } = require('../models/appModel');
 
 exports.blockApp = async (req, res) => {
   try {
-    return res.status(200).json(req.body);
     const app = await createApp(req.body);
-    if (!user) {
+    if (!app) {
       return res.status(404).json({ msg: 'User not found' });
     }
     res.status(200).json(app);
